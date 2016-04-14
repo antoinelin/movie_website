9de530f6ffb3fefdec122a816bf0563eed98c9d6
adrienZ <adrienzaganelli@gmail.com>
2016-04-15 00:19:44 +0200

    connect db

--- src/inc/database.php
+++ src/inc/database.php
@@ -1,25 +1,20 @@
 <?php
 
-define('DB_HOST','hervelinalin.mysql.db');
-define('DB_NAME','hervelinalin');
-define('DB_USER','hervelinalin');
-define('DB_PASS','KetchupT2');
-define('DB_ENC', 'utf-8');
+define('DB_HOST','localhost'); //your server
+define('DB_NAME','cannes_anniversary'); //your db
+define('DB_USER','root');
+define('DB_PASS','root'); // '' default on windows
 
 try
 {
     // Try to connect to database
-    $db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
+    $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
 
     // Set fetch mode to object
-    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
 }
 catch (Exception $e)
 {
     // Failed to connect
     die('Could not connect');
 }
-
-// //not sure why but it works
-// session_start();
