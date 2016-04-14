3a5de7604caed4caab3799f2c6d641adeadc4a13
FoxxNGU <antoine@lin.fr>
2016-04-15 00:13:24 +0200

    overflow

--- src/pages/edition.php
+++ src/pages/edition.php
@@ -1,29 +1,30 @@
+<main class="cd-index">
+  <ul class="home_project-indicators" id="myMenu">
+    <a href="<?= URL ?>edition#1"><li data-menuanchor="1" class="home_project-indicator"><span class="activeMenu"></span><p class="spanMenu1 activeText">EDITION</p></li></a>
+    <a href="<?= URL ?>edition#2"><li data-menuanchor="2" class="home_project-indicator"><span class="spanMenu2"></span><p class="spanMenu2">BUDGET</p></li></a>
+    <a href="<?= URL ?>edition#3"><li data-menuanchor="3" class="home_project-indicator"><span class="spanMenu3"></span><p class="spanMenu3">EARNINGS</p></li></a>
+    <a href="<?= URL ?>edition#4"><li data-menuanchor="4" class="home_project-indicator"><span class="spanMenu4"></span><p class="spanMenu4">GENDER</p></li></a>
+    <a href="<?= URL ?>edition#5"><li data-menuanchor="5" class="home_project-indicator"><span class="spanMenu5"></span><p class="spanMenu5">ORIGINS</p></li></a>
+    <a href="<?= URL ?>edition#6"><li data-menuanchor="6" class="home_project-indicator"><span class="spanMenu6"></span><p class="spanMenu6">CATEGORIES</p></li></a>
+  </ul>
+  <!-- <ul id="myMenu">
+      <li data-menuanchor="firstPage" class="active"><a href="#firstPage">First section</a></li>
+      <li data-menuanchor="secondPage"><a href="#secondPage">Second section</a></li>
+      <li data-menuanchor="thirdPage"><a href="#thirdPage">Third section</a></li>
+      <li data-menuanchor="fourthPage"><a href="#fourthPage">Fourth section</a></li>
+  </ul> -->
+  <div id="fullpage">
+      <div class="section first_one">
+        <div class="slide titre" >
+          <a href="<?= URL ?>#1/1">HELLO</a>
 
-<ul class="home_project-indicators" id="myMenu">
-  <a href="<?= URL ?>edition#1"><li data-menuanchor="1" class="home_project-indicator"><span class="activeMenu"></span><p class="spanMenu1 activeText">EDITION</p></li></a>
-  <a href="<?= URL ?>edition#2"><li data-menuanchor="2" class="home_project-indicator"><span class="spanMenu2"></span><p class="spanMenu2">BUDGET</p></li></a>
-  <a href="<?= URL ?>edition#3"><li data-menuanchor="3" class="home_project-indicator"><span class="spanMenu3"></span><p class="spanMenu3">EARNINGS</p></li></a>
-  <a href="<?= URL ?>edition#4"><li data-menuanchor="4" class="home_project-indicator"><span class="spanMenu4"></span><p class="spanMenu4">GENDER</p></li></a>
-  <a href="<?= URL ?>edition#5"><li data-menuanchor="5" class="home_project-indicator"><span class="spanMenu5"></span><p class="spanMenu5">ORIGINS</p></li></a>
-  <a href="<?= URL ?>edition#6"><li data-menuanchor="6" class="home_project-indicator"><span class="spanMenu6"></span><p class="spanMenu6">CATEGORIES</p></li></a>
-</ul>
-<!-- <ul id="myMenu">
-    <li data-menuanchor="firstPage" class="active"><a href="#firstPage">First section</a></li>
-    <li data-menuanchor="secondPage"><a href="#secondPage">Second section</a></li>
-    <li data-menuanchor="thirdPage"><a href="#thirdPage">Third section</a></li>
-    <li data-menuanchor="fourthPage"><a href="#fourthPage">Fourth section</a></li>
-</ul> -->
-<div id="fullpage">
-    <div class="section first_one">
-      <div class="slide titre" >
-        <a href="<?= URL ?>#1/1">HELLO</a>
-
+        </div>
+        <div class="slide" data-anchor="films"></div>
       </div>
-      <div class="slide" data-anchor="films"></div>
-    </div>
-    <div class="section"></div>
-    <div class="section"></div>
-    <div class="section"></div>
-    <div class="section"></div>
-    <div class="section"></div>
-</div>
+      <div class="section"></div>
+      <div class="section"></div>
+      <div class="section"></div>
+      <div class="section"></div>
+      <div class="section"></div>
+  </div>
+</main>
