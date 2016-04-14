92ba2e6d26722238f661db4f7f35e64f9d9398d0
ULYSSE TALLEPIED <ulysse.tallepied@hetic.net>
2016-04-14 13:23:56 +0200

    Revert "Merge branch 'master' into antoine"
    This reverts commit e706eaf639829a072c57f5484ed9aa39e8ded121, reversing
    changes made to 4a274c8b9dbd88f81e2578c063fde7b2f8745d35.

--- src/index.php
+++ src/index.php
@@ -1,19 +1,12 @@
 <?php
 
-<<<<<<< HEAD
 // Config
 include 'inc/options.php';
 include 'inc/config.php'; // Uncomment if you need database
-=======
-	// Config
-	 include 'php/config/database.php';
-	 include 'php/config/options.php';
->>>>>>> master
 
 // Get the query
 $q = empty($_GET['q']) ? '' : $_GET['q'];
 
-<<<<<<< HEAD
 // Routes
 if($q == '')
 	$page = 'home';
@@ -37,29 +30,3 @@ include 'controllers/'.$page.'.php';
 include 'partials/header.php';
 include 'pages/'.$page.'.php';
 include 'partials/footer.php';
-=======
-
-	// Get the query
-	$q = empty($_GET['q']) ? '' : $_GET['q'];
-
-	// Routes
-	if($q == '')
-		$page = 'home';
-	else if($q == 'editions' || preg_match('/^editions\/[0-9]+$/',$q))
-		$page = 'editions';
-	else if($q == 'dates')
-		$page = 'dates';
-	else if($q == 'retrospective')
-		$page = 'retrospective';
-	else
-		$page = '404';
-
-
-
-	include 'php/views/partials/header.php';
-
-  // include 'php/views/pages/years.php';
-  include 'php/views/pages/winners.php';
-
-  include 'php/views/partials/footer.php';
->>>>>>> master
