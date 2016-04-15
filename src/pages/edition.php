9c04eca17a39c7b236ec3b9a5b578336158cd7fd
Charles Mangwa <charlesmangwa@gmail.com>
2016-04-15 13:45:29 +0200

    Insert final charts in edition view #1

--- src/pages/edition.php
+++ src/pages/edition.php
@@ -2,39 +2,63 @@
   <ul class="home_project-indicators" id="myMenu">
     <a href="#1"><li data-menuanchor="1" class="home_project-indicator"><span class="spanMenu1 activeMenu"></span><p class="spanMenu1 activeText">EDITION</p></li></a>
     <a href="#2"><li data-menuanchor="2" class="home_project-indicator"><span class="spanMenu2"></span><p class="spanMenu2">BUDGET</p></li></a>
-    <a href="#3"><li data-menuanchor="3" class="home_project-indicator"><span class="spanMenu3"></span><p class="spanMenu3">EARNINGS</p></li></a>
-    <a href="#4"><li data-menuanchor="4" class="home_project-indicator"><span class="spanMenu4"></span><p class="spanMenu4">GENDER</p></li></a>
-    <a href="#5"><li data-menuanchor="5" class="home_project-indicator"><span class="spanMenu5"></span><p class="spanMenu5">ORIGINS</p></li></a>
-    <a href="#6"><li data-menuanchor="6" class="home_project-indicator"><span class="spanMenu6"></span><p class="spanMenu6">CATEGORIES</p></li></a>
+    <a href="#5"><li data-menuanchor="3" class="home_project-indicator"><span class="spanMenu3"></span><p class="spanMenu3">ORIGINS</p></li></a>
+    <a href="#6"><li data-menuanchor="4" class="home_project-indicator"><span class="spanMenu4"></span><p class="spanMenu4">CATEGORIES</p></li></a>
   </ul>
   <div id="fullpage" class="fp_background" data-year="2005">
       <div class="section first_one">
         <div class="slide moviesCompetition" data-anchor="hoverview">
           <a class="moviesFlip" href="#1/films_slide">MOVIES IN</br>COMPETITION</a>
           <h2 class="title_section"><span>2005</span></h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</br>Esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
         </div>
         <div class="slide hoverview" data-anchor="films_slide">
           <a class="hoverviewFlip" href="#1/hoverview">STATS</a>
         </div>
       </div>
-      <div class="section second_one">
+      <div class="section second_one" id="second_one">
         <h2 class="title_section"><span>2005</span></h2>
-        <h1><span>Yo</span></h1>
+          <article class="chart-description">
+            <div class="number">
+              <p class="data">19M</p>
+              <p>BUDGET</p>
+            </div>
+            <div class="text">
+              <p>
+                Lorem ipsum dolor sit amet, elit Festival de Cannes. Donec vitae risus semper, facilisis justo in, fermentum est. Lorem ipsum dolor sit amet, elit Festival de Cannes. Donec vitae risus semper, facilisis justo in, fermentum est.
+              </p>
+            </div>
+          </article>
           <canvas class="canvas" width="800" height="450"></canvas>
-      </div>
-      <div class="section third_one">
-        <h2 class="title_section"><span>2005</span></h2>
-      </div>
-      <div class="section fourth_one">
-        <h2 class="title_section"><span>2005</span></h2>
       </div>
       <div class="section fifth_one">
         <h2 class="title_section"><span>2005</span></h2>
+        <article class="chart-description">
+          <div class="number">
+            <p class="data">7</p>
+            <p>ORIGINS</p>
+          </div>
+          <div class="text">
+            <p>
+              Lorem ipsum dolor sit amet, elit Festival de Cannes. Donec vitae risus semper, facilisis justo in, fermentum est. Lorem ipsum dolor sit amet, elit Festival de Cannes. Donec vitae risus semper, facilisis justo in, fermentum est.
+            </p>
+          </div>
+        </article>
         <canvas class="canvas" width="800" height="450"></canvas>
       </div>
       <div class="section last_one">
         <h2 class="title_section"><span>2005</span></h2>
+        <article class="chart-description">
+          <div class="number">
+            <p class="data">10</p>
+            <p>CATEGORIES</p>
+          </div>
+          <div class="text">
+            <p>
+              Lorem ipsum dolor sit amet, elit Festival de Cannes. Donec vitae risus semper, facilisis justo in, fermentum est. Lorem ipsum dolor sit amet, elit Festival de Cannes. Donec vitae risus semper, facilisis justo in, fermentum est.
+            </p>
+          </div>
+        </article>
         <canvas class="canvas" width="800" height="450"></canvas>
       </div>
   </div>
