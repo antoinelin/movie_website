7b43a22fc95cb7f255eadb8b38f051982b011c4e
adrienZ <adrienzaganelli@gmail.com>
2016-04-15 16:26:35 +0200

    Link editions to edition #2

--- src/controllers/edition.php
+++ src/controllers/edition.php
@@ -34,6 +34,7 @@ foreach ($movies as $movie) {
   }
   //total origins
   if(empty($movie->country)){
+    $movie->country = "Unknown";
   }
   $stats["origins"][] =  $movie->country;
 
@@ -42,18 +43,53 @@ foreach ($movies as $movie) {
     $movie->genre = explode(",", $movie->genre );
     foreach ($movie->genre as $_genre) {
       $stats["genres_total"][]= $_genre;
+  //  echo   array_search($_genre, $movie->genre)."  |||||||||||||".count($movie->genre)."<br />";
     }
+  //  echo  "<br /><br /><br /><br /><br /><br /><br /><br />";
+
   }
 
 }
 
 if($count_earning > 0){
+  $stats["earning_total"] =  round($stats["earning"]/10000000).'M' ;
+  $stats["earning"] =  round($stats["earning"]/$count_earning/10000000).'M';
 }
+else {
+
+  $stats["earning"]  = "ND";
+  $stats["earning_total"] = "ND";
+}
+
 
 if($count_duration > 0){
   $stats["duration_total"] =  round($stats["duration"]);
   $stats["duration"] =  round($stats["duration"]/$count_duration);
 }
+else {
+  $stats["duration"]  = "ND";
+  $stats["duration_total"] = "ND";
+}
+  if(intval($stats["origins"]) > 0){
+    $stats["origins"] = count(array_count_values($stats["origins"]));
   }
+  else {
+    $stats["origins"] = 'ND';
+  }
+  if(intval($stats["genres_total"]) > 0){
+  //  echo "<pre>"; print_r((array_count_values($stats["genres_total"]))); echo "<pre>";
+
+    $stats["genres_total"] = count(array_count_values($stats["genres_total"]));
+  }
+  else {
+    $stats["genres_total"] = "ND";
+  }
+  if(intval($stats["budget_total"]) === 0){
+    $stats["budget_total"] = "ND";
+  }
+  else{
+    $stats["budget_total"] = $stats["budget_total"]."M";
+
   }
 
+//echo "<pre>"; print_r($stats["genres_total"]); echo "<pre>";
